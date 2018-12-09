664b2eb629b77c9a5f19eded706a15d52245211f
Nahid Hasan <30774439+nahid597@users.noreply.github.com>
2018-12-10 02:05:38 +0600

    my order page added.

--- src/app/my-orders/my-orders.component.ts
+++ src/app/my-orders/my-orders.component.ts
@@ -4,6 +4,7 @@ import { OrderService } from '../order.service';
 import { switchMap, map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
+
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
